661d612c47987db6b0df798e6dccf947b1ddfc2e
KaengEE <yopuiqwer@naver.com>
2023-11-16 15:53:27 +0900

    css수정

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,15 +7,14 @@ export const MovieList = (props) => {
       {props.movies.map((movie) => (
         <div className="image-container d-flex m-3" key={movie.imdbID}>
           <img src={movie.Poster} alt="movie"></img>
-
           <div
             onClick={() => props.handleClick(movie)}
-            className="overlay d-flex align-items-center justify-content-center"
+            className="overlay d-flex flex-column align-items-center justify-content-center"
           >
-            <span className="me-2">
-              {props.addMovie ? "선호작 추가" : "선호작제거"}
-            </span>
-            <span>{props.addMovie ? "💖" : "❌"}</span>
+            <p className="me-2">{movie.Title}</p>
+            <span className="me-2 ">
+              {props.add ? "선호작 추가 💖" : "선호작제거 ❌"}
+            </span>{" "}
           </div>
         </div>
       ))}
