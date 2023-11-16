68642c326e047533c7213ef872e62be493064551
KaengEE <yopuiqwer@naver.com>
2023-11-16 11:26:05 +0900

    초기영화데이터

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [movies, serMovies] = useState([
+    {
+      Title: "The Lord of the Rings: The Return of the King",
+      Year: "2003",
+      imdbID: "tt0167260",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+    },
+    {
+      Title: "The Lion King",
+      Year: "1994",
+      imdbID: "tt0110357",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BYTYxNGMyZTYtMjE3MS00MzNjLWFjNmYtMDk3N2FmM2JiM2M1XkEyXkFqcGdeQXVyNjY5NDU4NzI@._V1_SX300.jpg",
+    },
+    {
+      Title: "King Kong",
+      Year: "2005",
+      imdbID: "tt0360717",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMjYxYmRlZWYtMzAwNC00MDA1LWJjNTItOTBjMzlhNGMzYzk3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    },
+  ]);
   return <div>헬로우</div>;
 }
 export default App;
